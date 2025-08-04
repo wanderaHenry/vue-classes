78946d55a1dfcee5b545533b21a7bf9cddd70f16
wanderaHenry <henrywandera183@gmail.com>
2025-08-04 08:27:52 +0300

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <Hello />
+    <foota />
+  </div>
+</template>
+
+<script>
+import Hello from './components/greeting.vue'
+
+import foota from './components/foota.vue'
+
+export default {
+  components: {
+    Hello,
+    foota
+  }
+}
+</script>
+
+<style scoped>
+/*glabal styles*/
+div {
+  padding: 20px;
+}
+</style>
---
+++ src/components/greeting.vue
@@ -0,0 +1,21 @@
+<template>
+<div>
+<h1>I greet you guys! how are you doing all!</h1>
+</div>
+</template>
+
+
+<script>
+export default {
+  name: 'Greeting'
+}
+</script>
+
+
+<style scoped>
+h1 {
+  color: #42b883;
+  text-align: center;
+  margin-top: 20px;
+}
+</style>
