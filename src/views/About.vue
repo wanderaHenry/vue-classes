4c3a6a31098859d3f3f2fa7e82ad7633c3377c6c
wanderaHenry <henrywandera183@gmail.com>
2025-08-08 23:20:17 +0300

    vue directives

--- src/views/About.vue
+++ src/views/About.vue
@@ -3,6 +3,10 @@
     <h2>About FastSHOP</h2>
     <p>FastSHOP is the fastest online shopping platform where speed meets convenience. Shop with confidence and speed!</p>
   </main>
+  <!-- html directives
+  v-on
+  v-if
+  v-if-else -->
 </template>
 
 <script>
